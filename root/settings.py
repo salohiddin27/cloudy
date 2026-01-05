7a01d6660a86592edf7696c58cd2654a0d6b2a0d
salohiddin27 <salohiddinnurbayev25@gmail.com>
2026-01-06 01:14:16 +0500

    And finally, I fixed it

--- root/settings.py
+++ root/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -73,6 +74,6 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient",}
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient", }
     }
 }
